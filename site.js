3a073e3edd6f0d428b6fd4a4818970b8065ee2e2
Eric Thomas <eric.l.m.thomas@gmail.com>
2017-12-03 16:57:29 -0700

    updates

---
+++ site.js
@@ -0,0 +1,9 @@
+// The following code is based off a toggle menu by @Bradcomp
+(function() {
+  var burger = document.querySelector('.navbar-burger');
+  var menu = document.querySelector('.navbar-menu');
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+})();
